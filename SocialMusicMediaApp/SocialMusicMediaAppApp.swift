db69272209d064a6946b88e87b5595de5eb3213d
Christopher Sandoval Terry <Chrisandoterry64@gmail.com>
2024-08-22 18:03:13 -0500

    Initial Commit

---
+++ SocialMusicMediaApp/SocialMusicMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMusicMediaAppApp.swift
+//  SocialMusicMediaApp
+//
+//  Created by Christopher Sandoval Terry on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMusicMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
